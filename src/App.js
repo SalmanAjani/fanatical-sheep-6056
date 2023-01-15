94b65c3436d8791be0dfceeb146bdaffdce53b4c
Salman <salmanajani0426@gmail.com>
2023-01-15 11:33:07 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import AllRoutes from "./pages/AllRoutes";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <AllRoutes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/AllRoutes.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+
+const AllRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+    </Routes>
+  );
+};
+
+export default AllRoutes;
