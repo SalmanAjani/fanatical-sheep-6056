aa92ad42b33a02c1a0301ca942a163bcf5ec34a9
IAkashMondal <adas085322gmail.com>
2023-01-17 17:12:51 +0530

    Nimbel cart structure

---
+++ client-side/nimbelcart/src/Admin/components/AdminFooter.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AdminFooter = () => {
+  return (
+    <div>Admin.Footer</div>
+  )
+}
---
+++ client-side/nimbelcart/src/Admin/components/AdminNavbar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AdminNavbar = () => {
+  return (
+    <div>AdminNavbar</div>
+  )
+}
