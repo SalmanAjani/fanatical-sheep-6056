e3117a821155799a634f01ae672383e60bcae2b7
Salman <salmanajani0426@gmail.com>
2023-01-19 16:41:38 +0530

    added single product page

--- client-side/nimbelcart/src/Routes/AllRoutes.jsx
+++ client-side/nimbelcart/src/Routes/AllRoutes.jsx
@@ -1,35 +1,23 @@
-
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../Client/pages/Home/Home";
 import Products from "../Client/pages/Products/Products";
-
+import ProductDetails from "../Client/pages/Products/ProductDetails";
 
 import Cart from "../redux/bag/cart/cart";
 
-
 import AdminPage from "../Admin/Pages/AdminPage";
-
-
 
 export const AllRoutes = () => {
   return (
-    
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/products" element={<Products />}></Route>
+      <Route path="/ProductDetails" element={<ProductDetails />}></Route>
 
+      <Route path="/cart" element={<Cart />} />
 
-      <Route path="/cart" element={<Cart/>}/>
-
-
-      <Route path="/admin" element={<AdminPage/>}/>
-
-
+      <Route path="/admin" element={<AdminPage />} />
     </Routes>
-    
   );
 };
-
-
-
